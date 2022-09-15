fe9705b3015a0f99622dbbc3d884bcf289f6011c
Jean-Christophe Parent <jcpar27@ulaval.ca>
2022-09-14 20:10:39 -0400

    ok

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-
-function showExercice(){
-    document.getElementById("myExo").classList.toggle("show");
-}
-
 function showDropDown(){
-    
-    var inputElement=createInputElement();
+    var dropDownElement = document.getElementById("dropdown");
+    var inputElement = createInputElement();
+    var exo = ["Bench Press","Back Squat","Pull Up","Deadlift"];
     function createInputElement(){
         var element = document.createElement("input");
-
-
-
+        element.placeholder = "Recherche";
+        element.addEventListener("click",function(){
+            var dropdown = document.getElementById();
+            var options = createOptionElement(pets);
+        });
+        return element;
+    }
+    var clearElement = createClearElement();
+    function createClearElement(){
+        var element = document.createElement("span");
+        element.innerText = "X";
+        element.style.cursor = "pointer";
+        return element;
     }
 
+
+    function createOptionsElement(exercice){
+        var options = document.createElement("div");
+        options.id = "dropdown-exercice";
+    }
 
 }
